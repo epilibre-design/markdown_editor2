5b01b30f7a88b014e6f54c74eeb4faec133fc4d5
RastaPopoulos <rastapopoulos@spip.org>
2024-12-02 18:29:59 +0100

    fix: oups la coquille

--- markdown_editor_pipelines.php
+++ markdown_editor_pipelines.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 function markdown_editor_header_prive($flux) {
-	$flux .= '<script type="module" src="'.find_in_path('javascript/markdown_editor.dist.js').'"></javascript>';
+	$flux .= '<script type="module" src="'.find_in_path('javascript/markdown_editor.dist.js').'"></script>';
 	
 	return $flux;
 }
