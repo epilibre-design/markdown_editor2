6265159d44495c4f4e58819c9ffd8dd478050f2a
RastaPopoulos <rastapopoulos@spip.org>
2025-06-07 10:36:25 +0200

    feat: fichier de langue pour le paquet

---
+++ lang/paquet-markdown_editor_fr.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'markdown_editor_description' => 'Ce plugin permet d’écrire un texte sémantique (gras, citation, liste, etc) en WYSIWYG mais génère le tout au format Markdown + quelques ajouts propre à SPIP.',
+	'markdown_editor_slogan' => 'Éditeur WYSIWYG générant du texte en Markdown + modèles SPIP',
+	'markdown_editor_nom' => 'Éditeur de texte Markdown pour SPIP',
+];
