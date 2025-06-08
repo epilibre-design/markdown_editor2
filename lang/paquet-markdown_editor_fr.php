d10a8206d3ceec66775adfad109b8aed5f6af8e2
Salvatore <salvatore@rezo.net>
2025-06-09 01:30:03 +0200

    i18n: [Salvatore] [source:lang/ paquet-markdown_editor] Export depuis https://trad.spip.net de la langue fr i18n: [Salvatore] [source:lang/ paquet-markdown_editor] Mise a jour du bilan depuis https://trad.spip.net

--- lang/paquet-markdown_editor_fr.php
+++ lang/paquet-markdown_editor_fr.php
@@ -1,7 +1,11 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/markdown_editor.git
 
 return [
+
+	// M
 	'markdown_editor_description' => 'Ce plugin permet d’écrire un texte sémantique (gras, citation, liste, etc) en WYSIWYG mais génère le tout au format Markdown + quelques ajouts propre à SPIP.',
-	'markdown_editor_slogan' => 'Éditeur WYSIWYG générant du texte en Markdown + modèles SPIP',
 	'markdown_editor_nom' => 'Éditeur de texte Markdown pour SPIP',
+	'markdown_editor_slogan' => 'Éditeur WYSIWYG générant du texte en Markdown + modèles SPIP',
 ];
